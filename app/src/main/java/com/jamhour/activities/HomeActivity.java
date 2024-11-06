6e8925b562ca3cad777402108776b01142b29f8b
jamhour1g <yazanjam50@gmail.com>
2024-11-06 21:08:49 +0200

    feat: Update UI and logic, add exercise category list
    Enhance the UI and logic of the Home Screen activity by implementing a
    new list to display exercise categories.

--- app/src/main/java/com/jamhour/activities/HomeActivity.java
+++ app/src/main/java/com/jamhour/activities/HomeActivity.java
@@ -3,14 +3,30 @@ package com.jamhour.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.divider.MaterialDividerItemDecoration;
 import com.jamhour.R;
+import com.jamhour.android_util.CategoryRecyclerViewAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen_activity);
+        super.onCreate(savedInstanceState);
+
+        RecyclerView workOutCategories = findViewById(R.id.workOutCategoriesRecyclerView);
+        workOutCategories.setAdapter(new CategoryRecyclerViewAdapter());
+        workOutCategories.setLayoutManager(new LinearLayoutManager(this));
+        workOutCategories.addItemDecoration(
+                new MaterialDividerItemDecoration(
+                        this,
+                        MaterialDividerItemDecoration.VERTICAL
+                )
+        );
+
     }
+
 }
