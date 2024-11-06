d43d4e1d573987232eafd7b9b59e47fbb94175cc
jamhour1g <yazanjam50@gmail.com>
2024-11-07 01:31:03 +0200

    chore: Rename activity_exercises file to exercises_activity
    Rename `activity_exercises.xml` to `exercises_activity.xml` to align
    with the established naming convention for activity layout files.

--- app/src/main/java/com/jamhour/activities/ExercisesActivity.java
+++ app/src/main/java/com/jamhour/activities/ExercisesActivity.java
@@ -11,7 +11,7 @@ public class ExercisesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercises);
+        setContentView(R.layout.exercises_activity);
 
     }
 }
